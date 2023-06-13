a1208c912016e63e7ea119825fb8b6835abcc10f
Deep_Rohit <rohitdeep2003@gmail.com>
2023-06-13 14:07:21 +0530

    Add

--- src/app/basic/infrastructure/infrastructure.component.ts
+++ src/app/basic/infrastructure/infrastructure.component.ts
@@ -1,44 +1,72 @@
-import { Component, OnInit } from '@angular/core';
-import { HomeService } from 'src/app/core/services/home.services';
+import { Component, OnInit } from "@angular/core";
+import { HomeService } from "src/app/core/services/home.services";
 
 @Component({
-  selector: 'app-infrastructure',
-  templateUrl: './infrastructure.component.html',
-  styleUrls: ['./infrastructure.component.css']
+  selector: "app-infrastructure",
+  templateUrl: "./infrastructure.component.html",
+  styleUrls: ["./infrastructure.component.css"],
 })
 export class InfrastructureComponent implements OnInit {
-
   infraData: any = [];
-  constructor(
-    private homeService: HomeService
-  ) { }
+  multiImage: any = [];
+  mainData: any = [];
+  constructor(private homeService: HomeService) {}
 
   ngOnInit(): void {
     this.getInfraDataById();
   }
-  getInfraDataById() {
-    this.homeService.getImfraDetails(localStorage.getItem('InstituteId')).subscribe((res: any) => {
-      this.infraData = res;
-      this.infraData.forEach((element:any,index:any) => {
-        element.cols = false;
-      
+  getInfraMultiImages(id: any) {
+    this.homeService.getInfraMultiImageById(id).subscribe((res: any) => {
+      this.multiImage = res;
     });
-    })
   }
-  open(i:any){
-    this.infraData[i].cols = true;
-    this.infraData.forEach((element:any,index:any) => {
-      if(index==i){
+  getInfraDataById() {
+    this.homeService
+      .getInfraDetails(localStorage.getItem("InstituteId"))
+      .subscribe((res: any) => {
+        this.infraData = res;
+        debugger;
+        this.infraData.forEach((element: any) => {
+          if (element.id) {
+            this.homeService
+              .getInfraMultiImageById(element.id)
+              .subscribe((res: any) => {
+                this.multiImage = res;
+                this.mainData.push({
+                  id: element.id,
+                  institute_id: element.institute_id,
+                  infraTitle: element.infraTitle,
+                  infraDetails: element.infraDetails,
+                  infraImage: element.infraImage,
+                  createddate: element.createddate,
+                  updateddate: element.updateddate,
+                  multiImage: this.multiImage,
+                });
+                this.multiImage.push({
+                  image: element.infraImage,
+                });
+              });
+          }
+        });
+        this.mainData.forEach((element: any, index: any) => {
+          element.cols = false;
+        });
+      });
+  }
+  open(i: any) {
+    this.mainData[i].cols = true;
+    this.mainData.forEach((element: any, index: any) => {
+      if (index == i) {
         element.cols = true;
-      }else{
+      } else {
         element.cols = false;
       }
     });
   }
-  close(i:any){
-    this.infraData[i].cols = true;
-    this.infraData.forEach((element:any,index:any) => {
-    element.cols=false;
+  close(i: any) {
+    this.mainData[i].cols = true;
+    this.mainData.forEach((element: any, index: any) => {
+      element.cols = false;
     });
   }
 }
